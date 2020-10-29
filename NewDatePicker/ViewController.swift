03fe4544d06a878da6684ba364ca4db76ea1ef67
Renato Lopes <renato.lopes@eldorado.org.br>
2020-10-28 22:17:03 -0300

    Added  date picker outlet in ViewController

--- NewDatePicker/ViewController.swift
+++ NewDatePicker/ViewController.swift
@@ -8,12 +8,14 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    // MARK: - Outlets
+    @IBOutlet private weak var datePickerView: UIDatePicker!
+    
+    //MARK: - Lifecycle
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
-
 }
 
